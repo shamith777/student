d613af5dc01ee84fb14dd0f47e3bb77bcccb2751
shamith777 <142828883+shamith777@users.noreply.github.com>
2025-11-22 09:55:38 +0530

    Update s_details.py

--- s_details.py
+++ s_details.py
@@ -1,12 +1,12 @@
 import sys
 #Check if correct number of argurment
-if len(sys.argy) i=3:
+if len(sys.argv)i=3:
   print("Usuage: python student.py <name>")
   sys.exit(1)
 #sys.argy(0) is always the programname
-  script_name = sys.argy[0]
-  name = sys.argy[1]
-  rollno = sys.argy[2]
+  script_name = sys.argv[0]
+  name = sys.argv[1]
+  rollno = sys.argv[2]
 
 print("Script name:",script_name)
 print("Student Name:",name)
